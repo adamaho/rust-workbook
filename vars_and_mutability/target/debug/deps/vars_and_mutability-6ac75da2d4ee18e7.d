31fc4f720dab29a7ea59fe5f4b99d4923b1d0fb9
adamaho <aaho.public@gmail.com>
2020-03-07 12:03:24 -0500

    removing target

--- vars_and_mutability/target/debug/deps/vars_and_mutability-6ac75da2d4ee18e7.d
+++
@@ -1,5 +0,0 @@
-/Users/adam.aho/personal/rust-workbook/vars_and_mutability/target/debug/deps/vars_and_mutability-6ac75da2d4ee18e7.rmeta: src/main.rs
-
-/Users/adam.aho/personal/rust-workbook/vars_and_mutability/target/debug/deps/vars_and_mutability-6ac75da2d4ee18e7.d: src/main.rs
-
-src/main.rs:
